d150fb5caf65b03d9abf0d8a8dd7ba9c0420c2c4
Liam Golan <liamgolan@Liams-MacBook-Pro.local>
2024-06-17 17:58:44 +0300

    init case cobra project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add the MiniCssExtractPlugin to the webpack config
+    if (!isServer) {
+      config.plugins.push(new MiniCssExtractPlugin());
+    }
+
+    return config;
+  },
+  images: {
+    domains: ["utfs.io"]
+  }
+};
 
 export default nextConfig;
---
+++ src/components/HandleComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const HandleComponent = () => {
+    return (
+        <div className="w-5 h-5 rounded-full shdow border bg-white border-zinc-200 transition hover:bg-primary"/>
+    )
+}
+
+export default HandleComponent
